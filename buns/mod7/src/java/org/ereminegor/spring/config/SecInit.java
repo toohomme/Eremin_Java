7267ae7e0ad0e75e4b0a52a69df67a2b6c5b22af
toohomme <102966721+toohomme@users.noreply.github.com>
2023-11-19 20:30:07 +0500

    Add files via upload

---
+++ buns/mod7/src/java/org/ereminegor/spring/config/SecInit.java
@@ -0,0 +1,6 @@
+package org.ereminegor.spring.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecInit extends AbstractSecurityWebApplicationInitializer {
+}
---
+++ buns/mod7/src/webapp/WEB-INF/view/hr.jsp
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>HR</title>
+</head>
+
+<body>
+
+<h1>HR Info</h1>
+
+</body>
+</html>
